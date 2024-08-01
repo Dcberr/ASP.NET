d710c5fa673c56d72ba7129e88a0eb10bded48a7
Nguyễn Chiến Công <khongcanbiet941612@gmail.com>
2024-08-01 23:27:53 +0700

    Updated Login

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using api.Dtos.Account;
 using api.Interfaces;
@@ -8,6 +9,7 @@ using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -17,10 +19,12 @@ namespace api.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        private readonly SignInManager<AppUser> _signinManager;
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signinManager = signInManager;
         }
 
     [HttpPost("register")]
@@ -59,7 +63,29 @@ namespace api.Controllers
             return StatusCode(500, e);
         }
     }
+    
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login (LoginDto loginDto){
+        if(!ModelState.IsValid){
+            return BadRequest(ModelState);
+        }
+
+        var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.UserName);
+        
+        if(user == null) return Unauthorized("Invalid Username!");
+
+        var result = await _signinManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+        if(!result.Succeeded) return Unauthorized("Username not found and/or Password incorrect!");
+
+        return Ok(
+            new NewUserDto{
+                UserName = user.UserName,
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user)
+            }
+        );
     }
-
-
+    }
 }
